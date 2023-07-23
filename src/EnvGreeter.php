27f8c39bd9e7306a21a1872ee432dcfc2c82d2f2
lfernandoio <139306431+lfernandoio@users.noreply.github.com>
2023-07-23 01:07:38 -0400

    Update EnvGreeter.php

--- src/EnvGreeter.php
+++ src/EnvGreeter.php
@@ -3,7 +3,7 @@ namespace App;
 
 class EnvGreeter extends Greeter {
 	public function greetFromEnv(string $environmentVariableName):string {
-		$name =$environmentVariableName; //getenv($environmentVariableName);
+		$name =$getenv($environmentVariableName);
 		return $this->greet($name);
 	}
 }
